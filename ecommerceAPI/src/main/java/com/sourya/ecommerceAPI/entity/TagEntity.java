f148a190d7c6e95b48193454c9b00379e5996ccd
Sourya Bhattacharya <souryabhattacharyav3@gmail.com>
2023-03-19 18:51:54 +0530

    adding Entities

---
+++ ecommerceAPI/src/main/java/com/sourya/ecommerceAPI/entity/TagEntity.java
@@ -0,0 +1,38 @@
+package com.sourya.ecommerceAPI.entity;
+
+import jakarta.persistence.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tag")
+public class TagEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "ID", updatable = false, nullable = false)
+    private UUID id;
+
+    @NotNull(message = "Product name is required")
+    @Basic(optional = false)
+    @Column(name = "NAME")
+    private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public TagEntity setId(UUID id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TagEntity setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
